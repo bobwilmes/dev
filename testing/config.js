8e939fa3528f932b1ae79745ce8ea233d31cb5c6
Bob Wilmes <bobwilmes@gmail.com>
2021-12-06 13:16:03 -0700

    Update files

---
+++ testing/config.js
@@ -0,0 +1,6 @@
+// testing/config.js
+exports.config = {
+    "url": process.env.API_URL || "http://localhost:80",
+    "mongoClient": process.env.MONGO_URL || "mongodb://localhost:27017/bobwilmes",
+};
+
